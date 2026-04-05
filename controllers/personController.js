62b35864c94117ba250d712d7fb358821bc430c7
mhmd <mohammedashrafchiko@gmail.com>
2026-04-05 13:35:27 +0200

    Part 3: second commit vaildation task

--- controllers/personController.js
+++ controllers/personController.js
@@ -1,42 +1,60 @@
-import Person from '../models/person.js';
+import Person from "../models/person.js";
 
 export const getInfo = (req, res, next) => {
   Person.countDocuments({})
-    .then(count => {
-      res.send(`<p>Phonebook has info for ${count} people</p><p>${new Date()}</p>`);
+    .then((count) => {
+      res.send(
+        `<p>Phonebook has info for ${count} people</p><p>${new Date()}</p>`,
+      );
     })
-    .catch(error => next(error));
+    .catch((error) => next(error));
 };
 
 export const getAllPersons = (req, res, next) => {
-  Person.find({}).then(persons => res.json(persons)).catch(error => next(error));
+  Person.find({})
+    .then((persons) => res.json(persons))
+    .catch((error) => next(error));
 };
 
 export const getPerson = (req, res, next) => {
   Person.findById(req.params.id)
-    .then(person => person ? res.json(person) : res.status(404).end())
-    .catch(error => next(error));
+    .then((person) => (person ? res.json(person) : res.status(404).end()))
+    .catch((error) => next(error));
 };
 
 export const addPerson = (req, res, next) => {
   const { name, number } = req.body;
-  if (!name || !number) return res.status(400).json({ error: 'content missing' });
+  if (!name || !number)
+    return res.status(400).json({ error: "content missing" });
 
   const person = new Person({ name, number });
-  person.save().then(saved => res.json(saved)).catch(error => next(error));
+  person
+    .save()
+    .then((saved) => res.json(saved))
+    .catch((error) => next(error));
 };
 
 export const deletePerson = (req, res, next) => {
   Person.findByIdAndDelete(req.params.id)
     .then(() => res.status(204).end())
-    .catch(error => next(error));
+    .catch((error) => next(error));
 };
 
 export const updatePerson = (req, res, next) => {
   const { name, number } = req.body;
   const person = { name, number };
 
-  Person.findByIdAndUpdate(req.params.id, person, { new: true, runValidators: true, context: 'query' })
-    .then(updated => res.json(updated))
-    .catch(error => next(error));
+  Person.findByIdAndUpdate(req.params.id, person, {
+    new: true,
+    runValidators: true,
+    context: "query",
+  })
+    .then((updatedPerson) => {
+      if (updatedPerson) {
+        res.json(updatedPerson);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 };
--- models/person.js
+++ models/person.js
@@ -1,23 +1,39 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const url = process.env.URI;
 
-mongoose.connect(url)
-  .then(() => console.log('connected to MongoDB'))
-  .catch(err => console.log('error connecting to MongoDB:', err.message));
+mongoose
+  .connect(url)
+  .then(() => console.log("connected to MongoDB"))
+  .catch((err) => console.log("error connecting to MongoDB:", err.message));
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    minlength: 3,
+    required: true,
+  },
+  number: {
+    type: String,
+    minlength: 8,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return /^\d{2,3}-\d+$/.test(v);
+      },
+      message: (props) =>
+        `${props.value} is not a valid phone number! Use formats like 09-1234556 or 040-22334455.`,
+    },
+  },
 });
 
-personSchema.set('toJSON', {
+personSchema.set("toJSON", {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString();
     delete returnedObject._id;
     delete returnedObject.__v;
-  }
+  },
 });
-const Person = mongoose.model('Person', personSchema);
+const Person = mongoose.model("Person", personSchema);
 
 export default Person;
