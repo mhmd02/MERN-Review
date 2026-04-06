5bd2124553f344741f1dfa03a03d17691d32f9ad
mhmd <mohammedashrafchiko@gmail.com>
2026-04-06 11:09:08 +0200

    commit 4: increment blog likes

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -8,22 +8,38 @@ export const getAllBlogs = async (req, res) => {
 };
 
 export const createBlog = async (req, res) => {
-  const body = req.body
+  const body = req.body;
 
-  const user = await User.findOne({})
+  const user = await User.findOne({});
 
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes || 0,
-    user: user._id 
-  })
+    user: user._id,
+  });
 
-  const savedBlog = await blog.save()
-  
-  user.blogs = user.blogs.concat(savedBlog._id)
-  await user.save()
+  const savedBlog = await blog.save();
 
-  res.status(201).json(savedBlog)
-}
+  user.blogs = user.blogs.concat(savedBlog._id);
+  await user.save();
+
+  res.status(201).json(savedBlog);
+};
+
+export const likeBlog = async (req, res) => {
+  const { id } = req.params;
+
+  const updatedBlog = await Blog.findByIdAndUpdate(
+    id,
+    { $inc: { likes: 1 } },
+    { new: true, runValidators: true },
+  ).populate("user", { username: 1, name: 1 });
+
+  if (!updatedBlog) {
+    return res.status(404).json({ error: "blog not found" });
+  }
+
+  res.json(updatedBlog);
+};
--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
-import { getAllBlogs, createBlog } from "../controllers/blogController.js";
+import {
+  getAllBlogs,
+  createBlog,
+  likeBlog,
+} from "../controllers/blogController.js";
 
 const router = Router();
 
 router.get("/", getAllBlogs);
 router.post("/", createBlog);
+router.patch("/:id/like", likeBlog);
 
 export default router;
